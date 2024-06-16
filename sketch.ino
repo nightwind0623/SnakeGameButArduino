5c431727237f79a5a4f96bb62fb777af3b38b487
nightwind__ <115386737+nightwind0623@users.noreply.github.com>
2024-06-16 14:37:17 +0800

    Add files via upload
    The first finished version.

--- sketch.ino
+++ sketch.ino
@@ -4,32 +4,36 @@
 #include <Adafruit_SH1106.h> //https://github.com/wonho-maker/Adafruit_SH1106
 
 #define OLED_RESET -1
-#define LEFT 0
-#define RIGHT 1
-#define UP 2
-#define DOWN 3
 
-const int CELL_SIZE = 2;
-short xPin = 0;
-short yPin = 1;
-short zPin = 2;
+const byte CELL_SIZE = 2;
+byte xPin = 0;
+byte yPin = 1;
+byte zPin = 2;
+byte btnPin = 13;
 byte width = 128;
 byte height = 64;
 byte row = height / CELL_SIZE;
 byte column = width / CELL_SIZE;
 Adafruit_SH1106 screen(OLED_RESET);
 
+enum Direction {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN
+};
+
 class Node{
   private:
-    byte x;
-    byte y;
+    int x;
+    int y;
     Node* next;
   public:
-    byte getX(){return x;}
-    byte getY(){return y;}
+    int getX(){return x;}
+    int getY(){return y;}
     Node* getNext(){return next;}
     void setNext(Node* node){next = node;}
-    Node(byte x, byte y) : x(x), y(y), next(nullptr){}
+    Node(int x, int y) : x(x), y(y), next(nullptr){}
     Node() : x(0), y(0), next(nullptr){}
     ~Node(){
       x = 0;
@@ -40,7 +44,7 @@ class Node{
 class Snake{
   private:
     short bodySize;
-    char heading;
+    Direction heading;
     Node* headNode;
   public:
     void addNode(Node* newHead){
@@ -65,8 +69,8 @@ class Snake{
     void drawSnake(Adafruit_SH1106& screen){
       Node* currentNode = headNode;
       for(short i = 0; i < bodySize; i++){
-        byte x = currentNode->getX();
-        byte y = currentNode->getY();
+        int x = currentNode->getX();
+        int y = currentNode->getY();
         for(byte j = x; j < x + CELL_SIZE; j++){
           for(byte k = y; k < y + CELL_SIZE; k++){
             screen.drawPixel(j, k, WHITE);
@@ -91,9 +95,9 @@ class Snake{
         heading = DOWN;
       }
     }
-    char setHeading(char direction){
-      if(direction < 4 && direction > -1){
-        heading = direction;
+    void setHeading(Direction newDirection){
+      if(newDirection >= LEFT && newDirection <= DOWN){
+        heading = newDirection;
       }
     }
     char getHeading(){return heading;}
@@ -121,9 +125,9 @@ class Fruit{
   private:
     byte x;
     byte y;
-    bool checkOverlapping(int x, int y, Snake* snake){
+    bool checkOverlapping(byte x, byte y, Snake* snake){
       Node* currentNode = snake->getHeadNode();
-      for(int i = 0; i < snake->getBodySize(); i++){
+      for(short i = 0; i < snake->getBodySize(); i++){
         if(x == currentNode->getX() || y == currentNode->getY() ){
             return true;
         }
@@ -159,10 +163,36 @@ class Fruit{
     }
 };
 
+void restart(Snake*& oldSnake, Fruit* oldFruit){
+  delete oldSnake;
+  oldSnake = new Snake(50, 16);
+  oldSnake->addNode(new Node(50, 18));
+  oldSnake->addNode(new Node(50, 20));
+  oldFruit->setNewLocation(oldSnake);  
+}
+
+bool isGameover(Snake* snake){
+  Node* currentNode = snake->getHeadNode()->getNext();
+  Node* headNode = snake->getHeadNode();
+  for(short i = 1; i < snake->getBodySize(); i++){
+    if(headNode->getX() == currentNode->getX() && headNode->getY() == currentNode->getY()){
+      return true;
+    }
+    currentNode = currentNode->getNext();
+  }
+  if(headNode->getX() < 0 || headNode->getY() < 0 || headNode->getX() > width || headNode->getY() > height){
+    return true;
+  }
+  return false;
+}
+
 Snake* snake;
 Fruit* fruit;
 
+
 void setup()   {    
+  pinMode(btnPin, INPUT);
+
   snake = new Snake(50, 16);
   snake->addNode(new Node(50, 18));
   snake->addNode(new Node(50, 20));     
@@ -179,33 +209,49 @@ void setup()   {
 }
 
 void loop() {
-  screen.clearDisplay();
+  if(!isGameover(snake)){
+    screen.clearDisplay();
 
-  snake->changeDirection(xPin, yPin);
-  switch(snake->getHeading()){
-    case RIGHT:
-      snake->addNode(new Node(snake->getHeadNode()->getX() + CELL_SIZE, snake->getHeadNode()->getY()));
-      break;
-    case LEFT:
-      snake->addNode(new Node(snake->getHeadNode()->getX() - CELL_SIZE, snake->getHeadNode()->getY()));
-      break;
-    case UP:
-      snake->addNode(new Node(snake->getHeadNode()->getX(), snake->getHeadNode()->getY() + CELL_SIZE));
-      break;
-    case DOWN:
-      snake->addNode(new Node(snake->getHeadNode()->getX(), snake->getHeadNode()->getY() - CELL_SIZE));
-      break;
-  }
-  if(snake->getHeadNode()->getX() == fruit->getX() && snake->getHeadNode()->getY() == fruit->getY()){
-    fruit->setNewLocation(snake);
+    snake->changeDirection(xPin, yPin);
+    int newX = snake->getHeadNode()->getX();
+    int newY = snake->getHeadNode()->getY();
+    switch(snake->getHeading()){
+      case RIGHT:
+        newX += CELL_SIZE;
+        break;
+      case LEFT:
+        newX -= CELL_SIZE;
+        break;
+      case UP:
+        newY += CELL_SIZE;
+        break;
+      case DOWN:
+        newY -= CELL_SIZE;
+        break;
+    }
+    snake->addNode(new Node(newX, newY));
+
+    if(snake->getHeadNode()->getX() == fruit->getX() && snake->getHeadNode()->getY() == fruit->getY()){
+      fruit->setNewLocation(snake);
+    }else{
+      snake->removeNode();
+    }
+
+    snake->drawSnake(screen);
+    fruit->drawFruit(screen);
+    screen.display();
+    delay(200);
   }else{
-    snake->removeNode();
+    screen.clearDisplay();
+    screen.setTextSize(1);
+    screen.setTextColor(WHITE);
+    screen.setCursor(0, 0);
+    screen.print("press botton to \nrestart.\n\nyour score:");
+    screen.print(snake->getBodySize());
+    screen.display();
+    if(digitalRead(btnPin) == 1){
+      restart(snake, fruit);
+    }
   }
-
-  snake->drawSnake(screen);
-  fruit->drawFruit(screen);
-
-  screen.display();
-  delay(300);
 }
 
